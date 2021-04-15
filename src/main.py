2bc1f2b2734a99174e17a01180d6b8c9b31969a2
Luigiarminio <luigi.arminio@studenti.unitn.it>
2021-04-15 21:20:09 +0200

    Merge remote-tracking branch 'origin/master' 2
    # Conflicts:
    #	src/main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, annotations
 
+import json
 import time
 from datetime import datetime
 from typing import List
@@ -36,6 +37,74 @@ def get_stations(url: str, city: str) -> List[Station]:
 
 station_manager = StationManager()
 
+def get_totalAverage(file: str) -> None:
+    with open(file, "r") as f:
+        raw_stations = json.load(f)
+        station= [Station.from_repr(raw_station) for raw_station in raw_stations]
+    nb_bikes = 0
+    nb_slots = 0
+    for i in range(len(station)):
+        nb_bikes= nb_bikes + station[i].bikes
+        nb_slots= nb_slots + station[i].slots
+    bikes= nb_bikes/len(station)
+    parking=nb_slots/len(station)
+    print(bikes)
+    print(parking)
+station_manager = StationManager()
+
+trento_stations = get_stations_for_trento()
+rovereto_stations = get_stations_for_rovereto()
+
+stations = trento_stations + rovereto_stations
+get_totalAverage("stations.json")
+
+
+def get_totalAveragecity(file:str, city:str) -> None:
+    with open(file, "r") as f:
+        raw_stations = json.load(f)
+        station= [Station.from_repr(raw_station) for raw_station in raw_stations]
+    nb_bikes = 0
+    nb_slots = 0
+    for i in range(len(station)):
+        if city==station[i].city:
+            nb_bikes= nb_bikes + station[i].bikes
+            nb_slots= nb_slots + station[i].slots
+    bikes= nb_bikes/len(station)
+    parking=nb_slots/len(station)
+    print(bikes)
+    print(parking)
+station_manager = StationManager()
+
+trento_stations = get_stations_for_trento()
+rovereto_stations = get_stations_for_rovereto()
+
+stations = trento_stations + rovereto_stations
+get_totalAveragecity("stations.json","Trento")
+
+def get_totalAveragestation(file:str,name:str) -> None:
+    with open(file, "r") as f:
+        raw_stations = json.load(f)
+        station= [Station.from_repr(raw_station) for raw_station in raw_stations]
+    nb_bikes = 0
+    nb_slots = 0
+    for i in range(len(station)):
+        if name==station[i].name:
+            nb_bikes= nb_bikes + station[i].bikes
+            nb_slots= nb_slots + station[i].slots
+    bikes= nb_bikes/len(station)
+    parking=nb_slots/len(station)
+    print(bikes)
+    print(parking)
+station_manager = StationManager()
+
+trento_stations = get_stations_for_trento()
+rovereto_stations = get_stations_for_rovereto()
+
+stations = trento_stations + rovereto_stations
+get_totalAveragestation("stations.json","Piazza di Centa")
+
+
+"""
 while True:
 
     trento_stations = get_stations_for_trento()
@@ -50,3 +119,4 @@ while True:
     print("data updated!")
 
     time.sleep(60)
+"""
